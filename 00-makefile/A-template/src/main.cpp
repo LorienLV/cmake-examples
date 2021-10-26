f8c411f246e79bedfde3f08527eb454895aedeba
Lorien <lorienlopezv@gmail.com>
2021-10-26 18:43:13 +0200

    Better template

--- 00-makefile/A-template/src/main.cpp
+++ 00-makefile/A-template/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <cmath>
 
-int main(int argc, char *argv[])
-{
-   std::cout << "Hello CMake!" << std::endl;
-   return 0;
+#ifndef VALUE
+   #define VALUE 0
+#endif
+
+int main(int argc, char *argv[]) {
+   std::cout << "VALUE = " << VALUE << " " << sqrt((double)VALUE) << std::endl;
+
+   return EXIT_SUCCESS;
 }
